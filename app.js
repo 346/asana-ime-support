049152dbdad787961fade8b28e47fef85ba6270d
346 <346@ticketstreet.jp>
2014-07-08 22:16:58 +0900

    Fix bug

--- app.js
+++ app.js
@@ -4,12 +4,16 @@
   var keymap = KeyMap.prototype;
   keymap.__processKey = keymap.processKey;
   keymap.processKey = function(a, b) {
-    a.target().__isEnableIme = a._event.keyCode === 229;
+    var target = a.target();
+    var keyCode = a._event.keyCode;
+    // if (target.__isEnableIme === true && keyCode === 13) {
+    // }
+    target.__isEnableIme = keyCode === 229;
     this.__processKey(a, b); 
   };
 
   var view = window.HypertextInputView.MultiLine.prototype;
-    view.__addSelectionMarkersToDomIfFocused = view._addSelectionMarkersToDomIfFocused;
+  view.__addSelectionMarkersToDomIfFocused = view._addSelectionMarkersToDomIfFocused;
   view._addSelectionMarkersToDomIfFocused = function() {
     if (this.__isEnableIme === false) {
       return this.__addSelectionMarkersToDomIfFocused();
@@ -20,7 +24,7 @@
   node.__isEnableIme = false;
   node.__defaultHandler = node._defaultHandler;
   node._defaultHandler = function(a, b, c) {
-    if (b._model.isBar() && this.__isEnableIme === true) {
+    if (('_model' in b) && ('isBar' in b._model) && b._model.isBar() && this.__isEnableIme === true) {
       return Node.JUST_DEFAULT;
     } else {
       return this.__defaultHandler(a, b, c);
