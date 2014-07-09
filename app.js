d4caea3c1e7b197e75b280cc16793716aec8a338
346 <346@ticketstreet.jp>
2014-07-09 12:14:18 +0900

    Remove section hack temporary

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 (function() {
   "use strict";
 
+  // ime hack
   var keymap = KeyMap.prototype;
   keymap.__processKey = keymap.processKey;
   keymap.processKey = function(a, b) {
@@ -12,6 +13,7 @@
     this.__processKey(a, b); 
   };
 
+  // description hack
   var view = window.HypertextInputView.MultiLine.prototype;
   view.__addSelectionMarkersToDomIfFocused = view._addSelectionMarkersToDomIfFocused;
   view._addSelectionMarkersToDomIfFocused = function() {
@@ -20,15 +22,16 @@
     }
   };
 
-  var node = Node.prototype;
-  node.__isEnableIme = false;
-  node.__defaultHandler = node._defaultHandler;
-  node._defaultHandler = function(a, b, c) {
-    if (('_model' in b) && ('isBar' in b._model) && b._model.isBar() && this.__isEnableIme === true) {
-      return Node.JUST_DEFAULT;
-    } else {
-      return this.__defaultHandler(a, b, c);
-    }
-  };
+  // section hack
+  // var node = Node.prototype;
+  // node.__isEnableIme = false;
+  // node.__defaultHandler = node._defaultHandler;
+  // node._defaultHandler = function(a, b, c) {
+    // if (('_model' in b) && ('isBar' in b._model) && b._model.isBar() && this.__isEnableIme === true) {
+      // return Node.JUST_DEFAULT;
+    // } else {
+      // return this.__defaultHandler(a, b, c);
+    // }
+  // };
   
 }());
